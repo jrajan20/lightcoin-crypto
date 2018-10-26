7af08ca1bbdf600149064e34e8d2355e7ab19e57
jrajan20 <jai_20j@hotmail.com>
2018-10-26 22:26:24 +0000

    completed lightcoin excercise

--- index.js
+++ index.js
@@ -1,15 +1,87 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.transactions = [];
+  }
+
+  get balance(){
+  	let balance = 0;
+  	this.transactions.forEach((transact)=> {
+  		balance += transact.value;
+  	})
+  	return balance;
+  }
+
+  addTransaction(transaction){
+  	this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+  	this.time = new Date();
+  	if (this.isAllowed()){
+	  	this.time = new Date();
+	  	this.account.addTransaction(this)
+	  	return true;
+  	} else{
+  		return false;	
+  	}
+    
+    
   }
 
+ 
+
+
+}
+
+class Withdrawal extends Transaction{
+
+  // constructor(amount, account) {
+  //   this.amount = amount;
+  //   this.account = account;
+  // }
+   isAllowed(){
+  	if (this.account.balance > this.amount){
+  		return true;
+  	} else{
+  		console.log('Not enough funds!')
+  		return false;
+  	}
+  }
+
+  get value() {
+  	
+  		return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction{
+	
+	// constructor(amount, account) {
+ //    this.amount = amount;
+ //    this.account = account;
+ //  }
+  isAllowed(){
+  	return true;
+  }
+
+  get value() {
+    return this.amount;
+  }
 }
 
 
@@ -17,13 +89,22 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('snow-surfer');
+console.log('Starting Balance:', myAccount.balance);
 
-t1 = new Withdrawal(50.25);
+const t3 = new Deposit(120.00, myAccount);
+t3.commit();
+
+
+
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+
+const t2 = new Withdrawal(100.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+
+
+console.log('Balance:', myAccount.balance);
